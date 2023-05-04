e620abae7bee752cdc904844f516b90086620daa
angelillis.artem <angelillis.artem@studenti.salesianisesto.it>
2023-05-04 13:43:27 +0200

    Update

--- src/main/java/com/first/esgui/HelloApplication.java
+++ src/main/java/com/first/esgui/HelloApplication.java
@@ -11,8 +11,8 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
-        stage.setTitle("THIS IS AN WINDOW!");
+        Scene scene = new Scene(fxmlLoader.load(), 844, 390);
+        stage.setTitle("Dis isi an vindov!");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/first/esgui/HelloController.java
+++ src/main/java/com/first/esgui/HelloController.java
@@ -1,16 +1,35 @@
 package com.first.esgui;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 public class HelloController {
     @FXML
     private Label Login;
+    @FXML
+    private Label Register;
+    @FXML
+    private ChoiceBox choiceBox;
 
     @FXML
-    protected void onNumber1ButtonClick() {
+    protected void onLoginButtonClick() {
         Login.setText("Login non successo!");
+        Register.setText(" ");
+    }
+
+    @FXML
+    protected void onRegisterButtonClick() {
+        Register.setText("Registrazione non successa!");
+        Login.setText(" ");
+    }
+
+    @FXML
+    protected void onChoiceBoxClick() {
+        choiceBox.getItems().addAll("Anello", "Orecchino", "Bracciale");
     }
 
 }
